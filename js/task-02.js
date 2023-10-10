194bfd45884da30475341aabd9a6aeba61843fcf
Andrew Butenko <137400563+sedkol88@users.noreply.github.com>
2023-10-10 17:13:04 +0300

    task2 in progress
    task2 in progress

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+// console.log(ingredientsList);
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = [ingredient];
+  ingredientsList.append(item);
+  console.log(item);
+});
