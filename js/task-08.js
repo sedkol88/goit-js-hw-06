25a5af196eb6d912c5418601e6b8a6d1acd78d32
Andrew Butenko <137400563+sedkol88@users.noreply.github.com>
2023-10-13 23:26:37 +0300

    task08 is done
    task08 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const dataInput = {
+    email: email,
+    password: password,
+  };
+  console.log(dataInput);
+
+  form.reset();
+}
