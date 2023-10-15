f67275bb5ef7e363d23533e5c48d0c193ce4445b
Andrew Butenko <137400563+sedkol88@users.noreply.github.com>
2023-10-15 21:19:46 +0300

    task09 is done
    task09 is done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector("span.color");
+const btnEl = document.querySelector("button.change-color");
+
+bodyEl.setAttribute("style", "background-color: ");
+
+btnEl.addEventListener("click", (event) => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+});
