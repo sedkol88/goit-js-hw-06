aa2dd66fe96cbb3fd053033433addb4d11cc22b3
Andrew Butenko <137400563+sedkol88@users.noreply.github.com>
2023-10-17 00:47:54 +0300

    task10 is done
    task10 is done

--- js/task-02.js
+++ js/task-02.js
@@ -11,7 +11,7 @@ const ingredientsList = document.querySelector("#ingredients");
 let arrayOfItems = [];
 ingredients.forEach((ingredient) => {
   const item = document.createElement("li"); //створюю пункт списку
-  item.textContent = [ingredient]; //додаю текст до пункту списку
+  item.textContent = ingredient; //додаю текст до пункту списку
   item.classList.add("list-item"); //додаю клас до пункту списку
   arrayOfItems.push(item); //запушую кожен пункт до масиву, щоб потім всі пункти скопом додатии в ДОМ
 });
--- js/task-10.js
+++ js/task-10.js
@@ -4,9 +4,37 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-// const inputEl = document.querySelector("input");
+const inputEl = document.querySelector("input");
+const btnCreate = inputEl.nextElementSibling;
+const btnDestroy = btnCreate.nextElementSibling;
+const divBox = document.querySelector("#boxes");
 
-// function createBoxes(amount) {
-//   const valueInput = inputEl.value;
-//   console.log(valueInput);
-// }
+function createBoxes(event) {
+  const amount = inputEl.value;
+  // console.log(amount);
+
+  const numbers = [];
+  for (let i = 1; i <= amount; i += 1) {
+    numbers.push(i);
+  }
+  // console.log(numbers);
+
+  const markup = numbers
+    .map(
+      (number) =>
+        `<div style='background-color: ${getRandomHexColor()}; 
+        width: ${30 + (number - 1) * 10}px; 
+        height: ${30 + (number - 1) * 10}px'></div>`
+    )
+    .join("");
+  // console.log(markup);
+
+  divBox.insertAdjacentHTML("beforeend", markup);
+}
+
+function destroyBoxes(event) {
+  divBox.innerHTML = "";
+}
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
